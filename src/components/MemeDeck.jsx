13b2d828eff9f1288cd5e3b312f4b8a888536a4f
osas2211 <osaretin.frank10@gmail.com>
2022-05-04 14:04:26 +0100

    added mint nft btn

--- src/components/MemeDeck.jsx
+++ src/components/MemeDeck.jsx
@@ -3,18 +3,23 @@ import React from 'react'
 export default function MemeDeck({ memes }) {
     const deleteMeme = event => {
         if (!event.target.closest('.meme')) return;
-        event.target.remove();
+        event.target.parentElement.remove();
       };
   return (
-    <div  onClick={deleteMeme} className="meme-deck d-flex flex-wrap align-items-start">
+    <div  className="meme-deck d-flex flex-wrap align-items-start">
       <div className="meme-deck-scroll d-flex flex-wrap justify-content-center align-items-start">
         {
             memes.length !== 0 && memes.map((meme, id)=>(
-                <div className="meme bounceIn">
-                    <span className="header-text">{meme.header}</span>
-                    <span className="footer-text">{meme.footer}</span>
-                    <img src={meme.image} key={id} alt='meme'/>
-                </div>
+                <>
+                    <div className="meme bounceIn" key={id}>
+                        <div className='meme_details' onClick={deleteMeme}>
+                            <span className="header-text">{meme.header}</span>
+                            <span className="footer-text">{meme.footer}</span>
+                            <img src={meme.image} alt='meme'/>
+                        </div>
+                        <button className='btn btn-primary w-100 btn-lg text-uppercase'>mint nft</button>
+                    </div>
+                </>
             ))
         }
       </div>
